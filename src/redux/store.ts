55fab6257a03454ae0d8758d235402394716c963
saudkhanbpk <saudkhanbpk@gmail.com>
2024-11-04 04:05:25 -0800

    add redux toolkit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,15 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import authReducer from "./authSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
 
-// // Configure the Redux store
-// const store = configureStore({
-//   reducer: {
-//     auth: authReducer, // Use the authReducer in the store
-//   },
-// });
+// Configure the Redux store
+const store = configureStore({
+  reducer: {
+    auth: authReducer, // Use the authReducer in the store
+  },
+});
 
-// // Optional: Define types for RootState and AppDispatch for better TypeScript support
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
+// Optional: Define types for RootState and AppDispatch for better TypeScript support
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
-// export default store;
+export default store;
