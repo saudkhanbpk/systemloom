2f83cdecaad70be18d28d15e6b8659a215757ff0
Luqman khan <luqman.dagai@gmail.com>
2024-11-07 16:55:01 +0500

    next config set

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: import('next').NextConfig = {
   images: {
     domains: ['s3-alpha-sig.figma.com'], // Add the domain here
   },
