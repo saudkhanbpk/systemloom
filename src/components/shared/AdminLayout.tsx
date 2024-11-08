376edaab865d9544061ac7f35b068ab7b101dd7f
saudkhanbpk <saudkhanbpk@gmail.com>
2024-11-08 03:54:33 -0800

    admin panel changes

--- src/components/shared/AdminLayout.tsx
+++ src/components/shared/AdminLayout.tsx
@@ -1,28 +1,29 @@
 // app/admin/AdminLayout.tsx
 
-import React, { useMemo } from 'react';
-import AdminSidebar from './AdminSidebar';
-import AdminHeader from './AdminHeader';
+import React, { useMemo } from "react";
+import AdminSidebar from "./AdminSidebar";
+import AdminHeader from "./AdminHeader";
+// import AdminHeader from './AdminHeader';
 
 interface AdminLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
-    // Memoize AdminSidebar to prevent re-renders
-    const memoizedSidebar = useMemo(() => <AdminSidebar />, []);
+  // Memoize AdminSidebar to prevent re-renders
+  const memoizedSidebar = useMemo(() => <AdminSidebar />, []);
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <AdminHeader />
-            <div className="flex flex-1">
-                {memoizedSidebar} {/* Sidebar will not re-render */}
-                <main className="flex-1 p-4 w-100 overflow-auto">
-                    {children} {/* Only this part will change on page navigation */}
-                </main>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <AdminHeader />
+      <div className="flex flex-1">
+        {memoizedSidebar} {/* Sidebar will not re-render */}
+        <main className="flex-1 p-4 w-100 overflow-auto">
+          {children} {/* Only this part will change on page navigation */}
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayout;
