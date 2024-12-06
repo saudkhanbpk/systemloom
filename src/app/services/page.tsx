875fc2d4bacad24a63cec2d713dfacd8c66273c0
saudkhanbpk <saudkhanbpk@gmail.com>
2024-12-05 16:54:46 -0800

    fixed service page issue

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,18 +1,18 @@
-// "use client"
-// import ServicesSec from "@/components/services/Services"
-// import Head from "next/head"
+"use client"
+import ServicesSec from "@/components/services/Services"
+import Head from "next/head"
 
-// const Services = () => {
-//   return (
-//     <div>
-//        <Head>
-//         <link rel="canonical" href="https://www.techcreator.co/services" />
-//       </Head>
-//       <>
-//       <ServicesSec/>
-//       </>
-//     </div>
-//   )
-// }
+const Services = () => {
+  return (
+    <div>
+       <Head>
+        <link rel="canonical" href="https://www.techcreator.co/services" />
+      </Head>
+      <>
+      <ServicesSec/>
+      </>
+    </div>
+  )
+}
 
-// export default Services
+export default Services
