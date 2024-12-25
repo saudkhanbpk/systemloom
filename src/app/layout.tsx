70ec7105bbfbd30a192350cebaf77a01a47b408a
saudkhanbpk <saudkhanbpk@gmail.com>
2024-12-25 12:26:19 -0800

    change gtm id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -208,7 +208,7 @@ export default function RootLayout({
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-M7VGW76P');
+    })(window,document,'script','dataLayer','GTM-NSGC369J');
   `}
 </Script>
 
@@ -224,7 +224,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
   <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-M7VGW76P"
+    src="https://www.googletagmanager.com/ns.html?id=GTM-NSGC369J"
     height="0"
     width="0"
     style={{ display: "none", visibility: "hidden" }}
