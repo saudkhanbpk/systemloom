1b007caa8036dd51922fa57e19fa49f3311724ca
saudkhanbpk <saudkhanbpk@gmail.com>
2025-01-10 22:58:39 -0800

    add content in pricing page

--- src/components/pricing/PricingCards.tsx
+++ src/components/pricing/PricingCards.tsx
@@ -80,16 +80,16 @@ const PricingCards: React.FC = () => {
       <div>
        
 
-        <div className="flex flex-col md:flex-row justify-between  gap-16 py-10 md:py-10  px-2 lg:px-16 ">
-          <div className=" ">
-            <h2 className=" font-inter font-medium text-3xl md:text-[45.25px] leading-[1.2]">
+        <div className="flex flex-col md:flex-row justify-between  gap-16 py-10 md:py-10  md:px-8 px-3 lg:px-16 ">
+          <div className="md:w-[1900px] ">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl mb-2 ">
               Affordable Solutions Without Sacrificing Quality
             </h2>
-            <p className=" font-inter font-normal text-lg md:text-[20px] mt-4 md:mt-0">
+            <p className=" text-base md:text-lg lg:text-xl  mt-4 md:mt-0">
               Our pricing is structured to suit diverse business needs. From
               basic plans to premium solutions, we ensure that every client gets
               high-quality service, whether it's web development, digital
-              marketing, or app development.
+              marketing, or app development.We know every business has different and unique requirements, which provoke us to offer flexible options that cater to different budgets.We have a team of expertise committed to deliver exceptional results and add values to your investment. 
             </p>
           </div>
           <div className="relative sm:w-[400px] w-full h-[300px] md:w-[900px]  md:h-[349.42px] mx-auto md:mx-0">
@@ -97,62 +97,62 @@ const PricingCards: React.FC = () => {
               <Image
                 src={frontImage}
                 alt="Flexible pricing for high-quality web, marketing, and app development services"
-                className="absolute inset-0 rounded-2xl w-[600px]"
-                layout="fill"
-                objectFit="cover "
+                className="absolute inset-0 rounded-2xl lg:w-[600px]"
+                  layout="fill"
+                objectFit="cover"
               />
             </div>
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row justify-between gap-16 py-4 md:py-10 px-2 lg:px-16 ">
-          <div className="relative sm:w-[400px] w-full h-[300px] md:w-[900px]  md:h-[349.42px]  mx-auto md:mx-0">
+        
+
+        <div className="flex flex-col md:flex-row justify-between  gap-16 py-10 md:py-10  md:px-8 px-3 lg:px-16 ">
+          <div className="relative sm:w-[400px] w-full h-[300px] md:w-[900px]  md:h-[349.42px] mx-auto md:mx-0">
             <div className="relative z-10 w-full h-full">
               <Image
                 src={pricing2}
                 alt="Custom pricing that works around your business growth"
-                className="absolute inset-0 rounded-2xl w-[600px]"
-                layout="fill"
+                className="absolute inset-0 rounded-2xl lg:w-[600px]"
+                  layout="fill"
                 objectFit="cover"
               />
             </div>
           </div>
-          <div className=" ">
-            <h2 className="font-inter font-medium text-3xl md:text-[45.25px] leading-[1.2]">
-              Flexible Packages for All Businesses
+          <div className="md:w-[1900px] ">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl mb-2 ">
+            Flexible Packages for All Businesses
             </h2>
-            <p className="text-xl mt-4">
-              We offer customizable pricing plans to align with your goals.
-              Whether you need comprehensive solutions or specific services, our
-              packages are built to scale with your business as it grows.{" "}
+            <p className=" text-base md:text-lg lg:text-xl  mt-4 md:mt-0">
+            We offer customizable pricing plans to align with your goals. Whether you need comprehensive solutions or specific services, our packages are built to scale with your business as it grows.Our different packages ensure you pay for what you need, also providing with the option of upgradation or adjustment as we all know requirements evolve as time passes. We have a range of options that is suitable to any budget or project size. 
+Our pricing is transparent and there is no hidden fees.
             </p>
           </div>
+          
         </div>
         {/* Pricing form */}
         <div ref={pricingFormRef}>
           <PricingForm />
         </div>
 
-        <div className="flex flex-col md:flex-row justify-between gap-6 py-10 md:py-10 px-2 lg:px-16">
-          <div className=" ">
-            <h2 className=" font-inter font-medium text-3xl md:text-[45.25px] leading-[1.2]">
-              Why Choose TechCreator for Your Next Project?
+        
+
+       <div className="flex flex-col md:flex-row justify-between  gap-16 py-10 md:py-10  md:px-8 px-3 lg:px-16 ">
+          <div className="md:w-[1900px] ">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl mb-2 ">
+            Why Choose TechCreator for Your Next Project?
             </h2>
-            <p className=" font-inter font-normal text-lg md:text-[20px] mt-4 md:mt-0">
-              With TechCreator, you get more than just a service—you get a
-              partner committed to your success. Our pricing reflects our
-              dedication to delivering the best results while staying within
-              your budget.
+            <p className=" text-base md:text-lg lg:text-xl  mt-4 md:mt-0">
+With TechCreator, you get more than just a service—you get a partner committed to your success. Our pricing reflects our dedication to delivering the best results while staying within your budget.Understanding the market and business requirements make you to do something great. This is where TechCreator excels. We first understand your business goals and then deliver solution to tackle the challenges that come your way. We bring expertise and innovation to every project whether it is web development or app development.
             </p>
           </div>
-
-          <div className="relative sm:w-[400px] w-full h-[300px] md:w-[900px]  md:h-[349.42px]  mx-auto md:mx-0">
+          <div className="relative sm:w-[400px] w-full h-[300px] md:w-[900px]  md:h-[349.42px] mx-auto md:mx-0">
             <div className="relative z-10 w-full h-full">
               <Image
                 src={pricing1}
                 alt="TechCreator: A partner focused on your success with affordable pricing"
-                className="absolute inset-0 rounded-2xl w-[600px]"
-                layout="fill"
+                className="absolute inset-0 rounded-2xl lg:w-[600px]"
+                  layout="fill"
                 objectFit="cover"
               />
             </div>
