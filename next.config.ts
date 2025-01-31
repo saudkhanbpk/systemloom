d6ad8f2b8596ea0deb6ac4c9ab2c172e11fcaa9e
saudkhanbpk <saudkhanbpk@gmail.com>
2025-01-31 00:59:23 -0800

    update config.next.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,82 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['s3-alpha-sig.figma.com', 'res.cloudinary.com'], 
+    domains: ['s3-alpha-sig.figma.com', 'res.cloudinary.com'],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/services/app-development",
+        destination: "/services/mobile-app-development-company",
+        permanent: true, 
+      },
+      {
+        source: "/e-commerce-projects",
+        destination: "/e-commerce",
+        permanent: true,
+      },
+      {
+        source: "/services",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/services/qa-testing",
+        destination: "/services/web-qa-tester",
+        permanent: true,
+      },
+      {
+        source: "/services/graphic-designing",
+        destination: "/services/affordable-graphic-design-services",
+        permanent: true,
+      },
+      {
+        source: "/services/software-maintenance",
+        destination: "/services/software-maintenance-services",
+        permanent: true,
+      },
+      {
+        source: "/services/ui-ux-designing",
+        destination: "/services/ui-ux-design-agency",
+        permanent: true,
+      },
+      {
+        source: "/services/seo-content-writing",
+        destination: "/services/seo-services-for-small-business",
+        permanent: true,
+      },
+      {
+        source: "/services/sqa",
+        destination: "/services/web-qa-tester",
+        permanent: true,
+      },
+      {
+        source: "/services/ui-ux-design",
+        destination: "/services/ui-ux-design-agency",
+        permanent: true,
+      },
+      {
+        source: "/blog/blogDetail/best-practices-for-seo-in-2024:-boost-your-rankings-like-a-pro",
+        destination: "/",
+        permanent: true, 
+      },
+      {
+        source: "/blog/blogDetail/mastering-remote-collaboration-with-async-video-tools",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blog/blogDetail/exploring-the-impact-of-asynchronous-communication-on-productivity",
+        destination: "/",
+        permanent: true,
+      },
+    ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
